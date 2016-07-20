e6fd5a69980e47aba44b63335dbb6a309e87569b
Jordan Davidson <jordan.davidson@domo.com>
2016-07-19 18:26:25 -0600

    more stuff

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-particle compile photon main.ino SparkFunLSM9DS1/SparkFunLSM9DS1.h SparkFunLSM9DS1/SparkFunLSM9DS1.cpp SparkFunLSM9DS1/LSM9DS1_Registers.h SparkFunLSM9DS1/LSM9DS1_Types.h
+particle compile photon main.ino \
+  SparkFunLSM9DS1/SparkFunLSM9DS1.h \
+  SparkFunLSM9DS1/SparkFunLSM9DS1.cpp \
+  SparkFunLSM9DS1/LSM9DS1_Registers.h \
+  SparkFunLSM9DS1/LSM9DS1_Types.h
